93da4a442779b456242197407dd458775d60fa7a
MatteoNeri99 <matteolavoro99@gmail.com>
2024-06-27 07:50:40 +0200

    aggiunto array

---
+++ snack 3/index.php
@@ -0,0 +1,69 @@
+
+<?php 
+
+
+$posts = [
+
+    '23/12/2016' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Ferdinando Rolando',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Ferdinando Rolando',
+            'text' => 'Testo post 2'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Ferdinando Rolando',
+            'text' => 'Testo post 6'
+        ]
+    ],
+    '17/02/2018' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Ferdinando Rolando',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2022' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Ferdinando Rolando',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Ferdinando Rolando',
+            'text' => 'Testo post 5'
+        ],
+
+    ],
+];
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
